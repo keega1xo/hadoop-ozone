0374b391fb584c7c3f5a7cd2f9bb249ae46c225d
Márton Elek <elek@apache.org>
2019-10-18 22:04:14 +0200

    fix rebase action

--- .github/workflows/rebase.sh
+++ .github/workflows/rebase.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+TARGET_BRANCH=master
 git remote add apache https://github.com/apache/hadoop-ozone.git
-git fetch apache
+git fetch apache $TARGET_BRANCH
 git checkout -b work
-git rebase apache/master
-
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
 
-git push work:master --force-with-lease
+git rebase apache/$TARGET_BRANCH
+
+git remote add dest https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+git push dest work:$TARGET_BRANCH --force-with-lease
