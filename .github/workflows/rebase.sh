3f959cc34f9d10062eba530231a53759d785b8de
Márton Elek <elek@apache.org>
2019-10-18 21:54:09 +0200

    fix rebase script

--- .github/workflows/rebase.sh
+++ .github/workflows/rebase.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-git remote add apache https://github.com/elek/apache-hadoop.git
+git remote add apache https://github.com/apache/hadoop-ozone.git
 git fetch apache
+git checkout -b work
 git rebase apache/master
 
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
 
-git push --force-with-lease
+git push work:master --force-with-lease
