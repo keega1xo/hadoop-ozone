34766ce748f609cc898296626c5e581add438e3e
Márton Elek <elek@apache.org>
2019-11-05 18:39:40 +0100

    try to run tests with simple user

--- .github/buildenv/entrypoint.sh
+++ .github/buildenv/entrypoint.sh
@@ -5,4 +5,8 @@ if [ "$WITH_DOCKERD" ]; then
    chmod 777 /var/run/docker.sock
 
 fi
+ls -lah
+#fix permission which is not defined by github actions
+WHOAMI=$(id -u)
+sudo chown "$WHOAMI" -R  .
 "$@"
